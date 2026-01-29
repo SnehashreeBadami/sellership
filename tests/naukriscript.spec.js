85c2f0e8fec176fb26e2014583cc3af1819fccef
Snehashree <snehashreebn@gmail.com>
2026-01-29 12:09:01 +0530

    commit

--- tests/naukriscript.spec.js
+++ tests/naukriscript.spec.js
@@ -4,5 +4,5 @@ test("Naukri", async({page})=>{
     await page.locator('//a[@id="login_Layer"]').click();
     await page.getByPlaceholder("Enter your active Email ID / Username").fill("abc@gmaicom");
     await page.getByPlaceholder("Enter your password").fill("Sneha@123*!");
-    await page.locator('//button[@class="btn-primary loginButton"]').click()
+    await page.locator('//button[@class="btn-primary loginButton"]').click();
 })
