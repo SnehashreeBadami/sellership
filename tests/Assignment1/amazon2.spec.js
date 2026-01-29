911f7bdb17724c7d4d3f9de94e76b4400f657f33
Snehashree <snehashreebn@gmail.com>
2026-01-29 12:37:35 +0530

    initial commit

--- tests/Assignment1/amazon2.spec.js
+++ tests/Assignment1/amazon2.spec.js
@@ -4,4 +4,5 @@ test("Amazon2",async({page})=>{
     await page.locator('//input[@id="twotabsearchtextbox"]').fill("phones");
     await page.locator('//input[@id="nav-search-submit-button"]').click();
     await page.locator('//span[contains(text(),"Android 13.0")]/ancestor::div[@class="a-section"]/descendant::input[@type="checkbox"][72]').check();
+    await page.locator("cat")
 })
