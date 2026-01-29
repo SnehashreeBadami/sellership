346f2d828af1a42e9553a4878114d6e93f9f07be
Snehashree <snehashreebn@gmail.com>
2026-01-29 15:42:23 +0530

    commit

--- tests/googlescript.spec.js
+++ tests/googlescript.spec.js
@@ -3,6 +3,7 @@ test("Google",async({page})=>{
     await page.goto("https://www.google.com/")
     await page.locator('//textarea[@id="APjFqb"]').fill("Playwright Automation")
     await page.locator('//textarea[@id="APjFqb"]').press('Enter');
-    await page.locator('//span[@class="R1QWuf"]').nth(3).click()
-    await page.locator('//div[@class="kSFuOd rkqHyd"]').nth(3).click()
+    await page.locator('//span[@class="R1QWuf"]').nth(3).click();
+    await page.locator('//div[@class="kSFuOd rkqHyd"]').nth(3).click();
+
 })
