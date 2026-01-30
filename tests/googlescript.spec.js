36e3dd58b733324c0d1be071af68238a7c89821b
Snehashree <snehashreebn@gmail.com>
2026-01-30 10:48:25 +0530

    commit

--- tests/googlescript.spec.js
+++ tests/googlescript.spec.js
@@ -5,5 +5,6 @@ test("Google",async({page})=>{
     await page.locator('//textarea[@id="APjFqb"]').press('Enter');
     await page.locator('//span[@class="R1QWuf"]').nth(3).click();
     await page.locator('//div[@class="kSFuOd rkqHyd"]').nth(3).click();
+    await page.locator('cat');
 
 })
